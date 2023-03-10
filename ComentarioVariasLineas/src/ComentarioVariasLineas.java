592c2b4fbb94acb321998c9bcab01a026232783a
Alejandro Jimenez <ajimenezal01@gmail.com>
2023-03-10 10:19:10 +0100

    ENtrega archivos

---
+++ ComentarioVariasLineas/src/ComentarioVariasLineas.java
@@ -0,0 +1,15 @@
+
+public class ComentarioVariasLineas {
+
+	public static void main (String args []) {
+		/*System.out.println("Hola");
+		 * System.out.println("Adios");
+		 * System.out.printl("Cucu");*/
+		for(int i = 0 ; i<6;/*ahora viene el paso*/i++ ) {
+			System.out.println(i+"");
+			System.out.println("FIN DEL PROGRAMA");
+		}
+		
+	}
+	
+}
